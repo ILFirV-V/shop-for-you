b0307481e7ec85577261b9257a66395540421ec0
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2023-05-04 23:56:48 +0500

    детали продукта

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { IProduct} from "../../models/product";
+import { map, Observable } from "rxjs";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent {
+  product$: Observable<IProduct> | undefined;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.product$ = this.route.data.pipe(
+      map((data) => data['data'])
+    );
+  }
 }
