25854d1fa9cccdd4038604654328426dd27fd1e3
Сахабутдинов Ильфир <ilfir.sakhabutdinov@bk.ru>
2023-05-06 21:52:33 +0500

    подготовка к сортировке

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {IProduct} from "../models/product";
+import {Observable} from "rxjs";
 
 /**
  * Сервис для работы с продуктами.
@@ -17,7 +18,7 @@ export class ProductsService {
    * Получает список продуктов.
    * @returns Возвращает Observable с массивом продуктов.
    */
-  getProducts() {
+  getProducts(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(this.urlProducts);
   }
 
@@ -26,7 +27,22 @@ export class ProductsService {
    * @param id Идентификатор продукта.
    * @returns Возвращает Observable с объектом продукта.
    */
-  getProductById(id: number) {
+  getProductById(id: number): Observable<IProduct> {
     return this.http.get<IProduct>(`${this.urlProducts}/${id}`);
+  }
+
+  getProductsByCategory(category: string): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(`${this.urlProducts}/category/${category}`);
+  }
+
+  sortByFavoriteProducts(products: IProduct[]){
+  }
+
+  sortByAlphabet(products: IProduct[], order: string = 'asc'): IProduct[] {
+    return products.sort((a, b) => a.title.localeCompare(b.title) * (order === 'asc' ? 1 : -1));
+  }
+
+  sortByPrice(products: IProduct[], order: string = 'asc'): IProduct[] {
+    return products.sort((a, b) => (a.price - b.price) * (order === 'asc' ? 1 : -1));
   }
 }
